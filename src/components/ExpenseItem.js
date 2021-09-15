ecd6c0fe58e7dd0e0835b076628fcf236ff21737
unknown <kinchoily@hotmail.com>
2021-09-15 18:02:15 +0100

    moved markup for dates into separate component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,10 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.scss'
-
-//use either way 
-// function ExpenseItem() {
-//     return <h2>Expense item!</h2>
-// }
 
 const ExpenseItem = ({ date, title, amount }) => {
   //destructuring props.
 
-  //take these const below and move them into App.js and pass the data from App.js to here via props
+  //take the data below and move that into App.js and pass the data from App.js back into here via props
   // const expenseDate = new Date (2021, 9, 15);
   // const expenseTitle = 'Car insurance';
   // const expenseAmount = '294.50';
@@ -16,11 +12,7 @@ const ExpenseItem = ({ date, title, amount }) => {
   return (
     //can only have one! root element in this statement, but can wrap inside of another opening and closing div to work around it.
     <div className="expense-item">
-      <div>
-          <div>{date.getFullYear()}</div>
-          <div>{date.toLocaleString('en',{month:'long'})}</div>
-          <div>{date.toLocaleString('en',{day:'2-digit'})}</div>
-      </div>
+        <ExpenseDate date={date}/> {/* date comes from app.js but we are also passing that down into ExpenseDate component */}
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">£{amount}</div>
