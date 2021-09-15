6cd0af05934759e93548ce3c9b11a548242f789b
unknown <kinchoily@hotmail.com>
2021-09-15 17:34:13 +0100

    ExpenseItem.js passing dynamic values in with props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,17 +5,29 @@ import './ExpenseItem.scss'
 //     return <h2>Expense item!</h2>
 // }
 
-const ExpenseItem = () => {
-    return (
-      //can only have one! root element in this statement, but can wrap inside of another opening and closing div to work around it.
-      <div className="expense-item"> {/* looks like html but its actually JSX */}
-        <div>Sep 15th 2021</div>
-        <div className="expense-item__description">
-          <h2>Car insurance</h2>
-          <div className="expense-item__price">Amount</div>
-        </div>
+const ExpenseItem = ({ date, title, amount }) => {
+  //destructuring props.
+
+  //take these const below and move them into App.js and pass the data from App.js to here via props
+  // const expenseDate = new Date (2021, 9, 15);
+  // const expenseTitle = 'Car insurance';
+  // const expenseAmount = '294.50';
+
+  return (
+    //can only have one! root element in this statement, but can wrap inside of another opening and closing div to work around it.
+    <div className="expense-item">
+      <div>
+          <div>{date.getFullYear()}</div>
+          <div>{date.toLocaleString('en',{month:'long'})}</div>
+          <div>{date.toLocaleString('en',{day:'2-digit'})}</div>
       </div>
-    );
-} 
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">£{amount}</div>
+        {/* without destructuring (props.amount)*/}
+      </div>
+    </div>
+  );
+}; 
 
 export default ExpenseItem;
