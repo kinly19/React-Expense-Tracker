e3e255b1d94b40a3bf119c2ee49b5e506c2a5ad3
unknown <kinchoily@hotmail.com>
2021-09-21 16:15:32 +0100

    NewExpense.js Created a function to allow child-to-parent component communication (data from child up to parent)

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,12 +1,24 @@
 import './NewExpense.scss';
 import ExpenseForm from './ExpenseForm';
 
-const NewExpense = () => {
+const NewExpense = ({onAddExpense}) => {
+
+    //create a function which takes an argument (eneterdExpenseData) and spreads it. We pass this function down into our child component (expenseForm), 
+    //from our child component we call the function and pass in data as a parameter (expenseData). this way we can pass data back up from a child to a parent with a function. 
+    const onSaveExpenseDataHandler = (enteredExpenseData) => {
+        const expenseData = {
+            ...enteredExpenseData, //pull out all the key value pairs and add to this object
+            id: Math.random().toString()
+        };
+        console.log(expenseData);
+        onAddExpense(expenseData);
+    };
+
     return (
         <div className="new-expense">
-            <ExpenseForm />
+            <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler}/>
         </div>
     )
 };
 
-export default NewExpense; 
+export default NewExpense;   
