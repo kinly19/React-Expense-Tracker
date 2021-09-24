f3ccfa8e0f38e961bcfda61dd4ab0aab56c69895
unknown <kinchoily@hotmail.com>
2021-09-24 20:04:33 +0100

    Chart.js logic to get data to be passed down to ChartBar component

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -3,6 +3,10 @@ import './Chart.scss';
 import ChartBar from './ChartBar';
 
 const Chart = ({ dataPoints }) => {
+
+    const dataPointValues = dataPoints.map(dataPoint => dataPoint.value); //change datapoint object to just a number we can use
+    const totalMax = Math.max(...dataPointValues); //spread operator to pull out all the array elements and add them as standalone arguments to this max method.
+
   return (
     <div className="chart">
       {dataPoints.map((dataPoint) => (
